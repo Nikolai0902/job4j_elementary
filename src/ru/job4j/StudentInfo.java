e3de7ca3e11c0bb2e733e8cbf79edc476e29c034
Nikolai0902 <kbus94@yandex.ru>
2021-01-20 17:44:29 +0300

    3.3. Разбор первой программы. [#453268]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String[] args)  {
+        System.out.println("Nikolai Buslaev");
+        System.out.println("20.01.2021");
+    }
+}
