32bbe99413bef38691c768cdd3a84e534ad32132
Nikolai0902 <k1o2l3i49494>
2021-01-26 21:03:08 +0300

    0. Что такое тестирование [#453280]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,7 +1,7 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static intrubleToEuro(int value) {
+    public static int rubleToEuro(int value) {
         int rsl = value / 70;
         return rsl;
     }
@@ -15,5 +15,11 @@ public class Converter {
         int euro = Converter.rubleToEuro(140);
         int dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro are " + dollar + " dollar ");
+
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
     }
 }
